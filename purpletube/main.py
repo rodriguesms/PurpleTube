c8b9c9a2dbd5ad012f8fdb5a896d1278db6a0d27
Joelder Aguiar <joelderaguiar@eng.ci.ufpb.br>
2022-05-26 20:43:46 -0300

    rota filme e conexão banco

--- purpletube/main.py
+++ purpletube/main.py
@@ -8,7 +8,7 @@ app.include_router(movie.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
+# fun
 def start():
     """Launched with `poetry run start` at root level"""
     uvicorn.run("purpletube.main:app", host="0.0.0.0", port=8000, reload=True)
