ef86a7c1bd99d9394936b6b74bf4b7efd2d59d40
rodriguesms <miguelrodrigues@eng.ci.ufpb.br>
2022-06-16 17:39:04 -0300

    a

--- frontend/purpletube/src/components/SendComment/index.js
+++ frontend/purpletube/src/components/SendComment/index.js
@@ -1,31 +1,33 @@
 import "./style.css";
+import React, { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 import { MessageSquare } from "react-feather";
 import { api } from "../../services/api";
 import { useState } from "react";
 
 function SendComment({ codigo_usuario, codigo_filme }) {
-  const [comment, setComment] = useState();
-  function handleChange(event) {
-    const { value } = event.target;
-    setComment(value);
-  }
-  function handleSubmit(event) {
-    event.preventDefault();
-    const json_string = JSON.stringify({
+  const [comment, setComment] = useState("");
+  const navigate = useNavigate()
+  const d = new Date();
+
+  function handleSubmit() {
+    console.log(comment)
+    api.post("comments", {
       codigo_usuario: codigo_usuario,
       codigo_filme: codigo_filme,
-      "conteudo": "ótimo"
-    });
-    console.log(json_string);
-    api.post("comments", json_string).then((response) => {
-      console.log(response);
+      date: `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}`,
+      conteudo: comment
+    }).then((response) => {
+      api.get(`movies/movie_id/${codigo_filme}`).then((responseMovie) => {
+        navigate("/movie", { state: { movie: responseMovie.data}})
+      })
     });
   }
 
   return (
-    <form className="commentContainer" onSubmit={handleSubmit}>
-      <input id="sendComment" type="text" />
-      <button id="buttonComment" onChange={handleChange}>
+    <form className="commentContainer" onSubmit={() => handleSubmit()}>
+      <input id="sendComment" type="text" onChange={e => setComment(e.target.value)}/>
+      <button id="buttonComment" >
         <MessageSquare /> Comentar
       </button>
     </form>
--- frontend/purpletube/src/pages/MoviePage/index.js
+++ frontend/purpletube/src/pages/MoviePage/index.js
@@ -1,18 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import { useLocation } from 'react-router-dom';
 import { TopBar } from '../../components/TopBar'
+import { api } from "../../services/api";
+import DisplayComment from '../../components/DisplayComment/index'
+import SendComment from "../../components/SendComment";
 
 function MoviePage() {
 
   const movie = useLocation().state.movie
 
-   return (
+  const [user, setUser] = useState();
+  const [isLogged, setLogged] = useState(false);
+  const [loadingUser, setLoadingUser] = useState(true);
+  const [director, setDirector] = useState([""])
+  const [actors, setActors] = useState([])
+  const [loadDirector, setLoadDirector] = useState(true)
+  const [loadActor, setLoadActor] = useState(true)
+  const [comments, setComments] = useState([])
+  const [loadComments, setLoadComments] = useState(true)
+
+   useEffect(() => {
+
+        let loadUser = localStorage.getItem('user');
+        if(loadUser){
+          setLogged(true);
+          setUser(JSON.parse(loadUser));
+        }
+        setLoadingUser(false);
+
+        api.get(`/actor/movie_actors/${movie.codigo_filme}`).then((response) => {
+          setActors(response.data);
+          setLoadActor(false);
+        })
+
+        api.get(`/directors/movie_directors/${movie.codigo_filme}`).then((response) => {
+          setDirector(response.data);
+          setLoadDirector(false);
+        })
+
+        api.get(`comments/movie_comments/${movie.codigo_filme}`).then((response) => {
+          setComments(response.data)
+          setLoadComments(false);
+          console.log(comments)
+        })
+   }, []);
+
+  
+  return (
     <div className="pageContainer">
       <div className="topBar">
-        <TopBar isHome={false} />
+        <TopBar isHome={false}/>
       </div>
       <div className="body">
+        <div className="mid-area">
+          <div className="movieInfo-area">
+            <div className="individual-movie-image-card">
+              <img className="individual-movie-image" src={movie.baner}/>
+            </div>
+            <div className="mainMovieInfo">
+              <div style={{display: "flex", flexDirection: "column", width: "100%", alignItems: "center"}}>
+                <div className="movieTitle">{movie.nome_filme}</div>
+                <div className="directors">
+                  {
+                    !loadDirector &&
+                    director.map((director) => 
+                      <div className="movieDirector" key={director.codigo_diretor}>{director.nome_diretor}</div>
+                    )
+                  } 
+                </div>
+              </div>
+              <div className="movieDescription">{movie.descricao}</div>
+              <div className="actorsDiv">
+                {
+                  !loadActor &&
+                  actors.map((actor) => 
+                    <div className="actors" key={actor.codigo_ator}>{actor.nome_ator}</div>
+                  )
+                }
+              </div>
+            </div>
+          </div>
+          <div className="comments-area">
+            { isLogged && <SendComment codigo_filme={movie.codigo_filme} codigo_usuario={user.codigo_usuario}/> }
+            { !loadComments && 
+            comments.map((comment) => 
+              <DisplayComment key={comment.codigo_comentario} 
+              user={comment.nome_usuario} 
+              comment={comment.conteudo}
+              date={comment.data}/>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
